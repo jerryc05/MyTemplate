0f414096b76b4f25a933d31d357dd55e08bfab75
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-03-13 18:32:01 -0400

    update compiler flags, update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+  std::ios::sync_with_stdio(false);
   std::cout << "Hello, World!" << std::endl;
+  std::cout << argc << ',' << argv << std::endl;
   return 0;
 }
