0df4f0a3aa32e3d310a610cce6575c53a5ec9b45
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-05-01 01:46:11 -0400

    add static assert

--- src/utils.rs
+++ src/utils.rs
@@ -4,3 +4,13 @@
 pub(crate) const fn is_debug() -> bool {
   cfg!(debug_assertions)
 }
+
+#[macro_use]
+macro_rules! static_assert {
+  ($x:expr $(,)?) => {
+    const _: [
+      ();
+      0 - !{ const ASSERT: bool = $x; ASSERT } as usize
+    ] = [];
+  };
+}
