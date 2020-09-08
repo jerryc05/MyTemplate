aaec447b19609edb2dafc8b9e5c04fe74d77b91a
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-09-08 05:07:56 -0400

    add cin::tie()

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
-int main([[maybe_unused]] int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 #ifndef NDEBUG
   // debug mode
 #else
   // non-debug mode
   std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 #endif
 
   std::cout << "Hello, World!" << std::endl;
