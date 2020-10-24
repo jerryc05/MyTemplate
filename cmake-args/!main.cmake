13bb1985392b31783dde2e4db966b00acb5b2b42
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-10-24 16:20:48 -0400

    refactor use-latest-cpp-std

--- cmake-args/!main.cmake
+++ cmake-args/!main.cmake
@@ -64,19 +64,7 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")  # Last checked version: GCC 10
 
     ]]
 
-    message(CHECK_START "\t[USE LATEST C++ STD]")
-    if (__USE_LATEST_CPP_STD__)
-        execute_process(COMMAND ${CMAKE_CXX_COMPILER} -v --help
-                OUTPUT_VARIABLE __LATEST_CPP_STD__
-                ERROR_QUIET)
-        string(REGEX MATCHALL "-std=gnu\\+\\+[^9 ]+"
-                __LATEST_CPP_STD__ ${__LATEST_CPP_STD__})
-        list(GET __LATEST_CPP_STD__ -1 __LATEST_CPP_STD__)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${__LATEST_CPP_STD__}")
-        message(CHECK_PASS "ON: [${__LATEST_CPP_STD__}]")
-    else ()
-        message(CHECK_FAIL "OFF")
-    endif ()
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake-args/latest-std.cmake)
 
     #[[
 
@@ -322,20 +310,7 @@ elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")  # Last checked version: Clang 1
 
     ]]
 
-    message(CHECK_START "\t[USE LATEST C++ STD]")
-    if (__USE_LATEST_CPP_STD__)
-        execute_process(COMMAND ${CMAKE_CXX_COMPILER} -std= -xc++ -
-                ERROR_VARIABLE __LATEST_CPP_STD__)
-        string(REGEX MATCHALL "gnu[^']+"
-                __LATEST_CPP_STD__ ${__LATEST_CPP_STD__})
-        list(GET __LATEST_CPP_STD__ -1 __LATEST_CPP_STD__)
-        set(__LATEST_CPP_STD__ "-std=${__LATEST_CPP_STD__}")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${__LATEST_CPP_STD__}")
-        message(CHECK_PASS "ON: [${__LATEST_CPP_STD__}]")
-    else ()
-        message(CHECK_FAIL "OFF")
-    endif ()
-
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake-args/latest-std.cmake)
 
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
 -Wall \
