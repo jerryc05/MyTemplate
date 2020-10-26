52f0591aa702e2f987eaabb968220eec8068bf40
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-10-26 18:52:45 -0400

    remove scan-build; use clang-tidy w/ clang static analyzer

--- cmake-args/static-analyzer.cmake
+++ cmake-args/static-analyzer.cmake
@@ -1,5 +1,6 @@
 message(CHECK_START "\t[STATIC ANALYZER]")
 if (__USE_ANALYZER__)
+
     if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
 --param=analyzer-bb-explosion-factor=20 \
@@ -9,52 +10,26 @@ if (__USE_ANALYZER__)
 ")
 
     elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-
         unset(__CLANG_TIDY__ CACHE)
         find_program(__CLANG_TIDY__ clang-tidy)
 
         if (__CLANG_TIDY__)
             set(__CLANG_TIDY_ARGS__
-                    --use-color
-                    --checks=*,-android-*,-altera-*,-clang-analyzer*,-darwin-*,-fuchsia-*,-llvm-*,-llvmlibc-*,-objc-*,-zircon-*
+                    --allow-enabling-analyzer-alpha-checkers
+                    --checks=*,-android-*,-altera-*,-darwin-*,-fuchsia-*,-llvm-*,-llvmlibc-*,-objc-*,-zircon-*
                     --format-style=google
+                    --use-color
+
+                    --extra-arg=-Xclang
+                    --extra-arg=-analyzer-config
+                    --extra-arg=-Xclang
+                    --extra-arg=aggressive-binary-operation-simplification=true,c++-container-inlining=true,c++-shared_ptr-inlining=true,unroll-loops=true,widen-loops=true
                     )
             set(CMAKE_CXX_CLANG_TIDY ${__CLANG_TIDY__} ${__CLANG_TIDY_ARGS__})
 
         else ()
             message(WARNING "\t[STATIC ANALYZER] clang-tidy NOT FOUND! SKIPPED!")
         endif ()
-
-        #[[
-
-
-        ]]
-
-        file(READ_SYMLINK ${CMAKE_CXX_COMPILER} __ABS_COMPILER_PATH__)
-        if (NOT IS_ABSOLUTE "${__ABS_COMPILER_PATH__}")
-            get_filename_component(__COMPILER_DIR__ ${CMAKE_CXX_COMPILER} DIRECTORY)
-            set(__ABS_COMPILER_PATH__ "${__COMPILER_DIR__}/${__ABS_COMPILER_PATH__}")
-        endif ()
-        get_filename_component(__ABS_COMPILER_DIR__ ${__ABS_COMPILER_PATH__} DIRECTORY)
-
-        unset(__SCAN_BUILD__ CACHE)
-        find_program(__SCAN_BUILD__ scan-build
-                PATHS ${__ABS_COMPILER_DIR__})
-
-        if (__SCAN_BUILD__)
-            set(__SCAN_BUILD_ARGS__
-                    --force-analyze-debug-code
-                    -analyzer-config aggressive-binary-operation-simplification=true
-                    -enable-checker alpha
-                    -maxloop 16
-                    -o "../__analyzer-report")
-
-            set(CMAKE_CXX_COMPILER_LAUNCHER ${CMAKE_CXX_COMPILER_LAUNCHER} ${__SCAN_BUILD__} ${__SCAN_BUILD_ARGS__})
-
-        else ()
-            message(WARNING " \t[STATIC ANALYZER] scan-build NOT FOUND! SKIPPED!")
-        endif ()
-
 
     else ()
         message(SEND_ERROR "\t[STATIC ANALYZER] SWITCH UNIMPLEMENTED FOR THIS COMPILER CURRENTLY")
