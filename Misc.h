c1db1073cc7a5c89c7255b64599263cd19e3f520
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-11-02 01:14:27 -0500

    readability up; add cast define

--- Misc.h
+++ Misc.h
@@ -10,21 +10,32 @@
 #include <tuple>
 #include <vector>
 
+#define ccast const_cast
+#define rcast reinerpret_cast
+#define scast static_cast
+
 using F32 = float;
 using I16 = std::int16_t;
 using U32 = std::uint32_t;
 using Usize = std::size_t;
 
-template<typename T, Usize S> using Arr = std::array<T, S>;
-template<typename T> using Deque = std::deque<T>;
-template<typename K, typename V> using HashMap = std::unordered_map<K, V>;
+template<typename T, Usize S>
+using Arr = std::array<T, S>;
+template<typename T>
+using Deque = std::deque<T>;
+template<typename K, typename V>
+using HashMap = std::unordered_map<K, V>;
 using InputStream = std::istream;
-template<typename T1, typename T2> using Pair = std::pair<T1, T2>;
+template<typename T1, typename T2>
+using Pair = std::pair<T1, T2>;
 using String = std::string;
-template<typename... Ts> using Tuple = std::tuple<Ts...>;
-template<typename T> using Vec = std::vector<T>;
+template<typename... Ts>
+using Tuple = std::tuple<Ts...>;
+template<typename T>
+using Vec = std::vector<T>;
 
-template<typename T, typename Cmp=std::less<T>> using Pq = std::priority_queue<T, Vec<T>, Cmp>;
+template<typename T, typename Cmp=std::less<T>>
+using Pq = std::priority_queue<T, Vec<T>, Cmp>;
 
 auto inline skip_current_line(InputStream &is,
                               InputStream::int_type delim = '\n') -> void {
