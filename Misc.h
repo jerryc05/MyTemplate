7984f384afe786d86b813e096201c5552396f1c8
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-11-04 01:39:32 -0500

    add much more to Misc.h

--- Misc.h
+++ Misc.h
@@ -11,13 +11,19 @@
 #include <deque>
 #include <functional>
 #include <iostream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <experimental/optional>
 #include <queue>
 #include <tuple>
 #include <unordered_map>
+#include <utility>
+#include <variant>
 #include <vector>
 
 #define _ccast const_cast
-#define _rcast reinerpret_cast
+#define _rcast reinterpret_cast
 #define _scast static_cast
 
 using F32 = float;
@@ -26,11 +32,15 @@ using U32 = std::uint32_t;
 using Usize = std::size_t;
 
 #if (__GNUC__ >= 6) || (__clang_major__ >= 6)
-#define _nodiscard    [[nodiscard]]
 #define _maybe_unused [[maybe_unused]]
+#define _nodiscard    [[nodiscard]]
+#define _noexcept     noexcept
+#define _noreturn     [[noreturn]]
 #else
-#define _nodiscard
 #define _maybe_unused
+#define _nodiscard
+#define _noexcept
+#define _noreturn
 #endif
 
 #if __GNUC__ || __clang_major__ || _MSC_VER
@@ -44,21 +54,42 @@ template<typename T, Usize S>
 using Arr = std::array<T, S>;
 template<typename T>
 using Deque = std::deque<T>;
-template<typename K, typename V>
-using HashMap = std::unordered_map<K, V>;
+template<
+        typename Key,
+        typename Val,
+        typename Hash = std::hash<Key>,
+        typename KeyEq = std::equal_to<Key>,
+        typename Alloc = std::allocator<std::pair<const Key, Val> >
+>
+using HashMap = std::unordered_map<Key, Val, Hash, KeyEq, Alloc>;
 using InputStream = std::istream;
+template<typename T>
+using Optional = std::optional<T>;
 template<typename T1, typename T2>
 using Pair = std::pair<T1, T2>;
+template<
+        typename T,
+        typename Cont=std::vector<T>,
+        typename Cmp=std::less<typename Cont::value_type>
+>
+using Pq = std::priority_queue<T, Cont, Cmp>;
 using String = std::string;
+template<
+        typename Key,
+        typename Val,
+        typename Cmp = std::less<Key>,
+        typename Alloc = std::allocator<std::pair<const Key, Val> >
+>
+using TreeMap = std::map<Key, Val, Cmp, Alloc>;
 template<typename... Ts>
 using Tuple = std::tuple<Ts...>;
-template<typename T>
-using Vec = std::vector<T>;
-template<typename T, typename Cmp=std::less<T>>
-using Pq = std::priority_queue<T, Vec<T>, Cmp>;
+template<typename... Ts>
+using Variant = std::variant<Ts...>;
+template<typename T, typename Alloc = std::allocator<T>>
+using Vec = std::vector<T, Alloc>;
 
 
 auto inline skip_current_line(InputStream &is,
-                              InputStream::int_type delim = '\n') -> void {
+                              InputStream::int_type delim = '\n') {
   is.ignore(std::numeric_limits<std::streamsize>::max(), delim);
 }
