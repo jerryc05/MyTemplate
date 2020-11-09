404e0e34095c0d2a0595ca4b5d4442dcf6228e91
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-11-09 17:43:58 -0500

    update definition and add comments

--- Misc.h
+++ Misc.h
@@ -13,11 +13,15 @@
 #include <iostream>
 #include <map>
 #include <memory>
+
+// Workarounds
 #if __cplusplus >= 201703
 /**/#include <optional>
+
 #else
 /**/#include <experimental/optional>
 #endif
+
 #include <queue>
 #include <tuple>
 #include <unordered_map>
@@ -25,49 +29,55 @@
 #include <variant>
 #include <vector>
 
+// Casts
 #define _ccast    const_cast
 #define _dcast    dynamic_cast
 #define _rcast    reinterpret_cast
 #define _scast    static_cast
+
+// Asserts
 #define _sassert  static_assert
 
+// Annotations
 #if (__GNUC__ >= 6) || (__clang_major__ >= 6)
-/**/#define _maybe_unused [[maybe_unused]]
-/**/#define _nodiscard    [[nodiscard]]
-/**/#define _noexcept     noexcept
-/**/#define _noreturn     [[noreturn]]
+/**/#define MAYBE_UNUSED [[maybe_unused]]
+/**/#define NODISCARD    [[nodiscard]]
+/**/#define NOEXCEPT     noexcept
+/**/#define NORETURN     [[noreturn]]
 #else
-/**/#define _maybe_unused
-/**/#define _nodiscard
-/**/#define _noexcept
-/**/#define _noreturn
+/**/#define MAYBE_UNUSED
+/**/#define NODISCARD
+/**/#define NOEXCEPT
+/**/#define NORETURN
 #endif
 
+// Keywords
 #if __GNUC__ || __clang_major__ || _MSC_VER
-/**/#define _restrict __restrict
+/**/#define RESTRICT __restrict
 /**/#if __GNUC__ || __clang_major__
-/**//**/#define _inline_always  __attribute__((always_inline))
-/**//**/#define _inline_never   __attribute__((noinline))
+/**//**/#define INLINE_ALWAYS  __attribute__((always_inline))
+/**//**/#define INLINE_NEVER   __attribute__((noinline))
 /**/#else
-/**//**/#define _inline_always  __declspec(noinline)
+/**//**/#define INLINE_ALWAYS  __declspec(noinline)
 /**/#endif
 #else
-/**/#define _restrict
-/**/#define _inline_always inline_restrict
-/**/#define _inline_never
+/**/#define RESTRICT
+/**/#define INLINE_ALWAYS inline_restrict
+/**/#define INLINE_NEVER
 #endif
 
 
-using F32 = float;
-using I8 = std::int8_t;
-using I16 = std::int16_t;
-using I32 = std::int32_t;
-using I64 = std::int64_t;
-using U8 = std::uint8_t;
-using U16 = std::uint16_t;
-using U32 = std::uint32_t;
-using U64 = std::uint64_t;
-using Usize = std::size_t;
+using F32 MAYBE_UNUSED = float;
+using F64 MAYBE_UNUSED = double;
+using I8 MAYBE_UNUSED = std::int8_t;
+using I16 MAYBE_UNUSED = std::int16_t;
+using I32 MAYBE_UNUSED = std::int32_t;
+using I64 MAYBE_UNUSED = std::int64_t;
+using U8 MAYBE_UNUSED = std::uint8_t;
+using U16 MAYBE_UNUSED = std::uint16_t;
+using U32 MAYBE_UNUSED = std::uint32_t;
+using U64 MAYBE_UNUSED = std::uint64_t;
+using Usize MAYBE_UNUSED = std::size_t;
 
 
 template<typename T, Usize S>
@@ -108,7 +118,7 @@ template<typename T, typename Alloc = std::allocator<T>>
 using Vec = std::vector<T, Alloc>;
 
 template<typename Char>
-_inline_always auto skip_current_line(
+INLINE_ALWAYS auto skip_current_line(
         std::basic_istream<Char> &is,
         typename std::basic_istream<Char>::int_type delim = '\n') {
   is.ignore(std::numeric_limits<std::streamsize>::max(), delim);
