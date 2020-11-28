f4792f5cac5da2771cf57a670cfe566b330008b4
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-11-28 04:48:43 -0500

    update style

--- ParallelShell.sh
+++ ParallelShell.sh
@@ -3,13 +3,13 @@
 TMP_FILE=$(mktemp)
 NPROC=$(nproc)
 for i in $(seq 1 30); do
-  while jobs -r >${TMP_FILE}; (( $(cat ${TMP_FILE} | wc -l) > ${NPROC} )); do
+  while jobs -r >"$TMP_FILE"; (( $(wc -l <"$TMP_FILE") > NPROC )); do
     sleep .2
   done
 
   (
-    echo "${i}: Do sth in parallel here, for example"
-    ping 10.0.0.0 -w ${i}           >/dev/null
+    echo "$i: Do sth in parallel here, for example"
+    ping 10.0.0.0 -w "$i" >/dev/null
     # and much more ...
   ) &
 done
