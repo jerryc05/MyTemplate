3c3da9a206021a5aab6123327bc8ce6f5a22ed53
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-11-28 04:42:58 -0500

    update ParallelShell.sh

--- ParallelShell.sh
+++ ParallelShell.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 
-for i in $(seq 100 300); do
-  (( __ = __ % $(nproc) )); (( __++ == 0 )) && wait
+TMP_FILE=$(mktemp)
+NPROC=$(nproc)
+for i in $(seq 1 30); do
+  while jobs -r >${TMP_FILE}; (( $(cat ${TMP_FILE} | wc -l) > ${NPROC} )); do
+    sleep .2
+  done
 
   (
-    echo "Do sth in parallel here"
-    echo "and here"
+    echo "${i}: Do sth in parallel here, for example"
+    ping 10.0.0.0 -w ${i}           >/dev/null
+    # and much more ...
   ) &
 done
