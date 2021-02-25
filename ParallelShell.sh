62adce815d9159b3d20000e5cc5b0634b780e76a
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2021-02-25 01:05:00 -0500

    Update ParallelShell.sh

--- ParallelShell.sh
+++ ParallelShell.sh
@@ -2,13 +2,13 @@
 
 NPROC=$(nproc)
 for i in $(seq 1 30); do  
-  ((__++%NPROC==0)) && wait
+  while [ "$(jobs -r | wc -l)" -ge 3 ];do wait -n;done
 
-  (
+  {
     echo "$i: Do sth in parallel here, for example"
     ping 10.0.0.0 -w "$i" >/dev/null
     # and much more ...
-  ) &
+  }&
 done
 
 wait
