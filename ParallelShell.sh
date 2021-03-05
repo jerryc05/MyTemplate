441b206c0031c50a7a045e392bbdcabad84451eb
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2021-03-04 19:15:00 -0500

    Update ParallelShell.sh

--- ParallelShell.sh
+++ ParallelShell.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 red=$(tput setaf 1 2>/dev/null)
+grn=$(tput setaf 2 2>/dev/null)
 bld=$(tput bold 2>/dev/null)
 udl=$(tput smul 2>/dev/null)
 clr=$(tput sgr0 2>/dev/null)
 
-[ -z "$BASH" ]&&echo "${red}${bld}=====Test script may not work well under non-BASH shell\!=====" >&2
+[ -z "$BASH" ]&&echo "${red}${bld}=====Test script may not work well under non-BASH shell\!=====${clr}" >&2
 NPROC=$(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN)
 
-
+#for f in *.txt;do ...;done
 for i in $(seq 1 30); do
   while (($(jobs -r | wc -l)>=NPROC)); do wait -n; done
 
