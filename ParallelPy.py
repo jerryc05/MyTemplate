0e911610f92c96a1a6b94b228648274e319e1bcc
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2021-03-05 20:19:18 -0500

    Update ParallelPy.py

--- ParallelPy.py
+++ ParallelPy.py
@@ -3,6 +3,7 @@
 import multiprocessing as mp
 from multiprocessing.pool import Pool
 import os
+import shutil
 import sys
 from typing import TextIO
 
@@ -20,7 +21,7 @@ if __name__ == '__main__':
             pool.apply_async(run, (p, ))
 
     class Print:
-        BRIGHT, DIM, NORMAL, CLR_ALL = 1, 2, 22, 0
+        BRIGHT, DIM, NORMAL, CLR_ALL = '01', '02', '22', '00'
         BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, CLR_COLOR = 30, 31, 32, 33, 34, 35, 36, 37, 39
         L_BLACK_, L_RED_, L_GREEN_, L_YELLOW_, L_BLUE_, L_MAGENTA_, L_CYAN_, L_WHITE_ = 90, 91, 92, 93, 94, 95, 96, 97
 
@@ -28,24 +29,59 @@ if __name__ == '__main__':
             for name in dir(self):
                 if not name.startswith('_'):
                     setattr(self, name,
-                            '\033[' + str(getattr(self, name)) + 'm')
+                            '\x1b[' + str(getattr(self, name)) + 'm')
 
         def __call__(self,
                      *values: object,
                      sep: str = '',
                      end: str = '\n',
                      file: TextIO = sys.stdout,
-                     flush: bool = True):
-            print(*values,
-                  self.CLR_ALL,
-                  sep=sep,
-                  end=end,
-                  file=file,
-                  flush=flush)
+                     flush: bool = True,
+                     align: str = 'l',
+                     fill_ch: str = ' '):
+            if align != 'c' and align != 'r':
+                print(*values,
+                      self.CLR_ALL,
+                      sep=sep,
+                      end=end,
+                      file=file,
+                      flush=flush)
+            else:
+                orient = '^' if align == 'c' else '>'
+                cols, _ = shutil.get_terminal_size()
+                s = sep.join(list(map(str, values)))
+                f: str = '{:' + fill_ch + orient + str(cols + 5 *
+                                                       s.count('\x1b[')) + '}'
+                s = f.format(s)
+                print(s,
+                      self.CLR_ALL,
+                      sep=sep,
+                      end=end,
+                      file=file,
+                      flush=flush)
 
     p = Print()
+
     _N_PARALLEL = os.cpu_count() or 4
     p(p.YELLOW, 'Parallel count: ', p.BRIGHT, _N_PARALLEL)
+
+    _TERM_SIZE = shutil.get_terminal_size()
+    p(p.CYAN, 'Terminal size: ', p.BRIGHT, _TERM_SIZE.columns, ' col x ',
+      _TERM_SIZE.lines, ' ln')
+
+    p(123, align='r')
+    p(p.CYAN, 123, align='r')
+    p(p.CYAN, p.BRIGHT, 123, align='r')
+    p(p.CYAN, p.BRIGHT, 123, align='c')
+    p(p.CYAN,
+      'Terminal size: ',
+      p.BRIGHT,
+      _TERM_SIZE.columns,
+      ' col x ',
+      _TERM_SIZE.lines,
+      ' ln',
+      align='c')
+    exit()
     _pool = mp.Pool(_N_PARALLEL)
     p(p.MAGENTA, p.BRIGHT, '===== START! =====')
     schedule(_pool, p)
