bdf1596e834434e2eb37f949feb17b757b3082e3
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2021-03-25 01:56:40 -0400

    using signal to prevent infinite hanging

--- ParallelPy.py
+++ ParallelPy.py
@@ -3,12 +3,14 @@
 # Copyright (c) 2019-2021 Ziyan "Jerry" Chen (@jerryc05).
 #                         All rights reserved.
 
-import math
+from contextlib import suppress
+from math import ceil, floor
 import multiprocessing as mp
 from multiprocessing.pool import AsyncResult
 import os
-import pathlib
+from pathlib import Path
 import shutil
+import signal as sig
 import subprocess as sp
 import sys
 import time
@@ -17,48 +19,62 @@ from typing import Callable, Iterator, TextIO
 
 
 def run() -> 'tuple[bool, str, str, float]':
-    result, start_t = False, time.time()
-    test_name, reason = '', ''
+    def sig_handler(signum: 'sig.Signals', frame: 'sig.FrameType'):
+        raise TleErr
 
-    # TODO Begin here...
-    import random
-    n = random.random() * 2
-    result = n > 1
-    test_name = 'testTrue' if result else 'testFalse'
-    reason = 'TLE'
+    result, start_t, time_limit = False, time.time(), 2
+    test_name, reason = 'Sleep test', ''
     PROC_TASKS[mp.current_process().name] = test_name
+    sig.signal(sig.SIGALRM, sig_handler)  # use signal.SIG_IGN as handler to ignore
+    sig.alarm(time_limit)  # Only Unix  # use `signal.alarm(0)` to clear the alarm
 
-    # If you want to print something, don't forget to `align`
-    p(f'{GLOBAL_VAR} {n}s ...', align='l')
+    try:
+        # TODO Begin here...
+        import random
+        n = random.random() * 2.5
+        result = n <= 1
+        test_name = 'testTrue' if result else 'testFalse'
+        reason = 'Slept More Than 1 sec'
+        PROC_TASKS[mp.current_process().name] = test_name
 
-    time.sleep(n)
+        # If you want to print something, don't forget to `align`
+        p(f'{GLOBAL_VAR} {n}s ...', align='l')
 
-    p(f'{n}s sleeping done!', align='r')
+        time.sleep(n)
 
-    # If you want a real-time process call:
-    """
-    proc = sp.Popen(('ping', 'localhost'), stdout=sp.PIPE, stderr=sp.PIPE)
-    while ...:
-        line, poll = proc.stdout.readline(), proc.poll()
-        if not line and poll is not None:
-            if poll != 0:
-                result = False
-                reason = f'Subprocess Exited {poll}: {proc.stderr.read() if DBG_MODE else "*"}'
-            break  # Exited
-        # Do something below:
-        ...
-    """
+        p(f'{n}s sleeping done!', align='r')
 
-    # If you want a normal process call:
-    '''
-    stdout = sp.check_output(('ping', 'localhost'), stderr=sp.DEVNULL)
-    '''
+        # If you want a real-time process call:
+        """
+        proc = sp.Popen(('ping', 'localhost'), stdout=sp.PIPE, stderr=sp.PIPE)
+        while ...:
+            line, poll = proc.stdout.readline(), proc.poll()
+            if not line and poll is not None:
+                if poll != 0:
+                    result = False
+                    reason = f'Subprocess Exited {poll}: {proc.stderr.read() if DBG_MODE else "*"}'
+                break  # Exited
+            # Do something below:
+            ...
+        """
+
+        # If you want a normal process call:
+        '''
+        stdout = sp.check_output(('ping', 'localhost'), stderr=sp.DEVNULL)
+        '''
+
+        sig.alarm(0)
+    except TleErr:
+        with suppress(NameError):
+            proc.kill()  # remember to kill/term processes # pyright:reportUnboundVariable=false
+        result, reason = False, f'Time Limit {time_limit} sec Excceeded'
+
     PROC_TASKS[mp.current_process().name] = None
     return (result, test_name, reason, time.time() - start_t)
 
 
 def schedule() -> 'Iterator[tuple[Callable[..., object], tuple[object, ...]]]':
-    for _ in range(66):
+    for _ in range(33):
         yield (run, tuple())
 
 
@@ -67,10 +83,6 @@ def setup() -> None:
     GLOBAL_VAR = 'Sleeping'
 
 
-#
-#
-#
-#
 #
 #
 #
@@ -176,8 +188,12 @@ class Print:
                     lock.release()
 
 
-def find_file(name: str, parent: bool = True) -> 'pathlib.Path|None':
-    path_ = pathlib.Path(name)
+class TleErr(RuntimeError):
+    ...
+
+
+def find_file(name: str, parent: bool = True) -> 'Path|None':
+    path_ = Path(name)
     if path_.is_file():
         return path_.resolve()
     path_ = FILE_PATH.parent if parent else FILE_PATH
@@ -191,7 +207,7 @@ def find_file(name: str, parent: bool = True) -> 'pathlib.Path|None':
         for i, x in enumerate(g_res):
             s += f'\n{i+1}) \t{x}'
         raise RuntimeError(f'{s}{p.CLR_ALL}')
-    return pathlib.Path(g_res[0]).resolve()
+    return Path(g_res[0]).resolve()
 
 
 def strlen(s: str) -> int:
@@ -200,26 +216,28 @@ def strlen(s: str) -> int:
 
 if __name__ == '__main__':
     VERSION = 1
-    _N_PARALLEL = len(os.sched_getaffinity(0)) or 4
     DEF_TERM_SIZE = (60, -1)
     _term_sz = shutil.get_terminal_size(DEF_TERM_SIZE)
-    FILE_PATH = pathlib.Path(__file__).parent.resolve()
+    FILE_PATH = Path(__file__).parent.resolve()
     DBG_MODE = os.environ.get('DBG')
+    n_cores = os.cpu_count() or 4
+    with suppress(AttributeError):
+        n_cores = len(os.sched_getaffinity(0))
 
     p = Print()
     try:
-        mp.set_start_method('fork')
+        mp.set_start_method('fork')  # Only Unix
     except ValueError:
         raise NotImplementedError(f'{p.RED}Unsupported Operating System!{p.CLR_ALL}')
     lock = mp.RLock()
     p(
-        f"{p.CYAN}v{VERSION}\tParallel count: {p.BOLD}{_N_PARALLEL}\t{p.NORMAL}Terminal window size: {p.BOLD}{_term_sz[0] if _term_sz != DEF_TERM_SIZE else '?'} x {_term_sz[1] if _term_sz != DEF_TERM_SIZE else '?'}"
+        f"{p.CYAN}v{VERSION}\tCPU core(s): {p.BOLD}{n_cores}\t{p.NORMAL}Term size: {p.BOLD}{_term_sz[0] if _term_sz != DEF_TERM_SIZE else '?'} x {_term_sz[1] if _term_sz != DEF_TERM_SIZE else '?'}"
     )
 
     setup()
     tasks = tuple(schedule())
     PROC_TASKS: 'dict[str, str|None]' = mp.Manager().dict()
-    with mp.Pool(max(1, min(_N_PARALLEL, len(tasks)))) as pool:
+    with mp.Pool(max(1, min(n_cores, len(tasks)))) as pool:
         rets: 'list[AsyncResult[tuple[bool, str, str, float]]]' = []
         succ, fail = tp.cast('list[list[tuple[str, str, float]]]', ([], []))
         print(end=f'{p.MAGENTA}{p.BOLD}')
@@ -233,7 +251,7 @@ if __name__ == '__main__':
         proc_tasks = PROC_TASKS.copy()
         while rets:
             for x in rets[:]:
-                try:
+                with suppress(mp.TimeoutError):
                     res = x.get(timeout=0)
                     (succ if res[0] else fail).append(tuple(res[1:]))
                     rets.remove(x)
@@ -246,8 +264,8 @@ if __name__ == '__main__':
                     else:
                         s += f'{p.BOLD}{p.RED}{res[1]} {p.NORMAL}... {"ERR!"}'
 
-                    p1, proc_tasks = math.floor(cols * percent), PROC_TASKS
-                    p2 = math.floor((cols*percent - p1) * len(prog_bars))
+                    p1, proc_tasks = floor(cols * percent), PROC_TASKS
+                    p2 = floor((cols*percent - p1) * len(prog_bars))
                     p3 = cols - p1 - (1 if p2 else 0)
                     max_proc_name = len(max(proc_tasks.keys(), key=len))
 
@@ -265,8 +283,6 @@ if __name__ == '__main__':
                             f'\r{hint}  {prog_bars[-1]*p1}{prog_bars[p2] if p2 else ""}{prog_bars[0]*p3}  {percent:7.2%} - {n_rets-len(rets):{dg_rets}}/{n_rets:{dg_rets}}{p.CUR_UP*(3+len(proc_tasks))}\r',
                             align='l'
                         )
-                except mp.TimeoutError:
-                    continue
         p('\n' * (3 + len(proc_tasks)), align='l')
 
     if succ or fail:
@@ -276,12 +292,12 @@ if __name__ == '__main__':
         digit, sec_digit = len(str(max(len(succ), len(fail)))), 4
         for i, (x, r, t) in enumerate(succ):
             p(
-                f'{p.GREEN}{i+1:>{digit}}. OK ({t:{sec_digit}.{max(0,sec_digit-1-len(str(math.ceil(t))))}f} s) {p.BOLD}{x}{p.NORMAL}'
+                f'{p.GREEN}{i+1:>{digit}}. OK ({t:{sec_digit}.{max(0,sec_digit-1-len(str(ceil(t))))}f} s) \u2714 {p.BOLD}{x}{p.NORMAL}'
             )
         i = 1
         for i, (x, r, t) in enumerate(fail):
             p(
-                f'{p.RED}{i+1:>{digit}}.ERR ({t:{sec_digit}.{max(0,sec_digit-1-len(str(math.ceil(t))))}f} s) {p.BOLD}{x}{p.NORMAL} \t({r})'
+                f'{p.RED}{i+1:>{digit}}.ERR ({t:{sec_digit}.{max(0,sec_digit-1-len(str(ceil(t))))}f} s) \u274C {p.BOLD}{x}{p.NORMAL} \t({r})'
             )
         res = f'{p.GREEN}PASSED: {p.BOLD}{len(succ)}{p.NORMAL}  {p.RED}FAILED: {p.BOLD}{len(fail)}'
         res_len = strlen(res)
