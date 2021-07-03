69e33be31b8120fb4b993322365eb77f7a8c9b79
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2021-07-03 00:26:36 -0400

    Update .env-vars.sh

--- .env-vars.sh
+++ .env-vars.sh
@@ -3,14 +3,11 @@
 [ -d "$HOME/.local/bin" ] && export PATH="$HOME/.local/bin:$PATH"
 
 # Cargo
-[ -d $HOME/.cargo/bin ] && export PATH="$HOME/.cargo/bin:$PATH"
+[ -d "$HOME/.cargo/bin" ] && export PATH="$HOME/.cargo/bin:$PATH"
 
 # LLVM Clang for Mac
-if [ "$(uname -s)" = "Darwin" ] && [ -d "/usr/local/opt/llvm" ]; then
-  export PATH="/usr/local/opt/llvm/bin:$PATH"
-  [ -f /usr/local/opt/llvm/bin/llvm-clang   ] || ln -s /usr/local/opt/llvm/bin/clang   /usr/local/opt/llvm/bin/llvm-clang
-  [ -f /usr/local/opt/llvm/bin/llvm-clang++ ] || ln -s /usr/local/opt/llvm/bin/clang++ /usr/local/opt/llvm/bin/llvm-clang++
-
-      __lib="-L/usr/local/opt/llvm/lib"     && { [[ ! "$LFDLAGS"  == *"$__lib"*     ]] && export  LDFLAGS="$LDFLAGS $__lib";      unset __lib }
-  __include="-I/usr/local/opt/llvm/include" && { [[ ! $"CPPFLAGS" == *"$__include"* ]] && export CPPFLAGS="$CPPFLAGS $__include"; unset __include }
+if [ "$(uname -s)" = "Darwin" ] && [ -d "$(brew --prefix llvm)" ]; then
+  export PATH="$(brew --prefix llvm)/bin:$PATH"  
+      __lib="-L$(brew --prefix llvm)/lib"     && { [[ ! "$LFDLAGS"  == *"$__lib"*     ]] && export  LDFLAGS="$LDFLAGS $__lib";      unset __lib }
+  __include="-I$(brew --prefix llvm)/include" && { [[ ! $"CPPFLAGS" == *"$__include"* ]] && export CPPFLAGS="$CPPFLAGS $__include"; unset __include }
 fi
